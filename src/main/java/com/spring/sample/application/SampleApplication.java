012d9fe5050aae4e246f3cc91409d6533492b10e
sparshgupta2212 <sparsh.gupta_cs18@gla.ac.in>
2022-11-11 16:39:09 +0530

    Swagger

--- src/main/java/com/spring/sample/application/SampleApplication.java
+++ src/main/java/com/spring/sample/application/SampleApplication.java
@@ -2,8 +2,10 @@ package com.spring.sample.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class SampleApplication {
 
 	public static void main(String[] args) {
